a26f91f6aa20c25a35d44ad10229e276363c66ce
Taiwobello <bellotaiwolateef1999@gmail.com>
2023-02-16 09:46:40 +0100

    add api helper

---
+++ src/utils/helpers/rest-api-config.ts
@@ -0,0 +1,36 @@
+const baseUrl = "";
+export const pushNotificationUrl = "https://fcm.googleapis.com/fcm/send";
+
+interface FetchInstanceType {
+  get: (url: string, config?: RequestInit) => Promise<any>;
+  post: (url: string, data?: any, config?: RequestInit) => Promise<any>;
+}
+
+const processResponse = async (response: Response) => {
+  const json = await response.json();
+  // Reject 4xx and 5xx responses
+  if (/^(4|5)/.test(String(response.status))) {
+    throw json;
+  }
+  return json;
+};
+
+export const restAPIInstance: FetchInstanceType = {
+  get: (url, config) =>
+    fetch(`${baseUrl}${url}`, {
+      ...(config || {}),
+      method: "GET",
+      headers: {
+        ...(config?.headers || {}),
+      },
+    }).then(processResponse),
+  post: (url, data, config) =>
+    fetch(`${baseUrl}${url}`, {
+      body: JSON.stringify(data),
+      ...(config || {}),
+      method: "POST",
+      headers: {
+        ...(config?.headers || {}),
+      },
+    }).then(processResponse),
+};
