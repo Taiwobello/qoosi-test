f1f3d90a937462f7a0e952535471aba51acc4d2a
Taiwobello <bellotaiwolateef1999@gmail.com>
2023-02-16 18:25:07 +0100

    style country list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import CountryCars from "./components/countryCard/CountryCard";
+import { allCountries } from "./utils/helpers/data/country";
+import { Country } from "./utils/helpers/types/Country";
+import styles from "./App.module.scss";
+
 function App() {
-  return <h1>Qoosi Test</h1>;
+  const [allCountriesData, setAllCountriesData] = useState<Country[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchAllCountries = async () => {
+    setLoading(true);
+    const { data, error } = await allCountries();
+
+    if (!error && data) {
+      setAllCountriesData(data);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchAllCountries();
+  }, []);
+
+  console.log(allCountriesData);
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Qoosi Test</h1>
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className={styles["country-wrapper"]}>
+          {allCountriesData.map((country, index) => (
+            <CountryCars key={index} country={country} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/countryCard/CountryCard.tsx
@@ -0,0 +1,38 @@
+import { Country } from "../../utils/helpers/types/Country";
+import styles from "./CountryCard.module.scss";
+
+interface CountryCardProps {
+  country: Country;
+}
+
+export default function CountryCars(props: CountryCardProps) {
+  const { country } = props;
+  const { altSpellings, cca2, cca3, flag, idd, name } = country;
+
+  return (
+    <div className={styles.card}>
+      <img src={flag.png} alt={flag.alt} />
+      <div className={styles.details}>
+        <p>
+          <strong>Name:</strong> <span>{name.official}</span>{" "}
+        </p>
+        <p>
+          <strong>Alt Name: </strong> <span>{altSpellings}</span>{" "}
+        </p>
+        <p>
+          <strong>Code: </strong>{" "}
+          <span>
+            {idd.root}
+            {idd.suffixes && idd.suffixes[0]}
+          </span>
+        </p>
+        <p>
+          <strong>Character Code:</strong> <span>{cca2}</span>{" "}
+        </p>
+        <p>
+          <strong>Character Code:</strong> <span>{cca3}</span>{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
